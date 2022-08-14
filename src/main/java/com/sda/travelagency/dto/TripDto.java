acd59ed78ff1c9b7c980b9442093fb81ce249ed9
samaneh <s.khanimirzaee@gmail.com>
2022-08-14 13:58:51 +0300

    [change] - Trip Dto - in progress

--- src/main/java/com/sda/travelagency/dto/TripDto.java
+++ src/main/java/com/sda/travelagency/dto/TripDto.java
@@ -1,4 +1,29 @@
 package com.sda.travelagency.dto;
 
-public record TripDto() {
+import com.sda.travelagency.entity.Destination;
+import com.sda.travelagency.entity.HotelFacilities;
+import com.sda.travelagency.entity.Price;
+import com.sda.travelagency.entity.SecurityRules;
+import com.sda.travelagency.entity.enumeration.MealType;
+import com.sda.travelagency.entity.enumeration.PaymentType;
+import com.sda.travelagency.entity.enumeration.TransportType;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
+public record TripDto( LocalDate tripStartDate,
+                       LocalDate tripEndDate,
+                       Destination destination,
+                       Price price,
+                       TransportType typeOfTransport,
+                       SecurityRules securityRules,
+                       PaymentType paymentType,
+                       MealType mealType,
+                       HotelFacilities hotelFacilities,
+                       List<String> photos) {
+
+
 }
+
