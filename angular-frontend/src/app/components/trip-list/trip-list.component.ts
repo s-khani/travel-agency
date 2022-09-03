56fa20b96d4adfb220bb1cadd8ffa361e72a8dc9
samaneh <s.khanimirzaee@gmail.com>
2022-09-03 12:42:04 +0200

    [change] - material design library- added to project

--- angular-frontend/src/app/components/trip-list/trip-list.component.ts
+++ angular-frontend/src/app/components/trip-list/trip-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-trip-list',
-  templateUrl: './trip-list.component.html',
+  templateUrl: './trip-list.ng add @angular/materialcomponent.html',
   styleUrls: ['./trip-list.component.css']
 })
 export class TripListComponent implements OnInit {
