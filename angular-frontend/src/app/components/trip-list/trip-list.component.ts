fae0a505e2e2ccbea87565c7202ff1fe30f7131b
samaneh <s.khanimirzaee@gmail.com>
2022-09-03 12:50:17 +0200

    [change] - dummy content for trip list nice dog :))

--- angular-frontend/src/app/components/trip-list/trip-list.component.ts
+++ angular-frontend/src/app/components/trip-list/trip-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-trip-list',
-  templateUrl: './trip-list.ng add @angular/materialcomponent.html',
+  templateUrl: './trip-list.component.html',
   styleUrls: ['./trip-list.component.css']
 })
 export class TripListComponent implements OnInit {
