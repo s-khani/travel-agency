4fd31d3bb3e2e80c9c9095d8dec1638d5aa52dc3
samaneh <s.khanimirzaee@gmail.com>
2022-09-10 12:28:42 +0200

    [change] - entity or mapping- in progress

--- backend/src/main/java/com/sda/travelagency/entity/Trip.java
+++ backend/src/main/java/com/sda/travelagency/entity/Trip.java
@@ -5,6 +5,7 @@ import com.sda.travelagency.entity.enumeration.PaymentType;
 import com.sda.travelagency.entity.enumeration.TransportType;
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -12,16 +13,30 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor (access = AccessLevel.PRIVATE)
 @Builder
+@Entity
 public class Trip {
+    @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     LocalDate tripStartDate;
     LocalDate tripEndDate;
+
+    //dirty fix just ignore that field
+    @Transient
     Destination destination;
+    @Transient
     Price tripPrice;
+    @Enumerated(EnumType.STRING)
     TransportType typeOfTransport;
+    @Transient
     SecurityRules securityRules;
+    @Enumerated(EnumType.STRING)
     PaymentType paymentType;
+    @Enumerated(EnumType.STRING)
     MealType mealType;
+    @Transient
     HotelFacilities hotelFacilities;
+    @Transient
     List<String> photos;
 
 }
---
+++ backend/src/main/java/com/sda/travelagency/repository/RealTripRepository.java
@@ -0,0 +1,8 @@
+package com.sda.travelagency.repository;
+
+import com.sda.travelagency.entity.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RealTripRepository extends JpaRepository<Trip,Long> {
+
+}
