845edc6a2bcf0f2e58f1d233151af3b8e3d92347
samaneh <s.khanimirzaee@gmail.com>
2022-09-18 13:29:39 +0200

    [change] - form in progress - need to be fixed :)

--- angular-frontend/src/app/components/navigation/navigation.component.ts
+++ angular-frontend/src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {aboutUsUrl, homeUrl, tripsUrl} from "../../models/urls";
+import {aboutUsUrl, homeUrl, newTripUrl, tripsUrl} from "../../models/urls";
 
 @Component({
   selector: 'app-navigation',
@@ -11,6 +11,7 @@ export class NavigationComponent {
   home = homeUrl
   aboutUs = aboutUsUrl
   trips = tripsUrl
+  newTrip = newTripUrl
 
 
 
